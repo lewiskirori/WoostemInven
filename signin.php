01e43580369316aad80ff0ae166b42037b962d68
Lewis K Ycot <88181151+lewiskirori@users.noreply.github.com>
2023-02-21 04:51:45 -0800

    Update signin.php

--- signin.php
+++ signin.php
@@ -55,6 +55,8 @@ if($_POST) {
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>Log in: Woostem</title>
 
 	<!-- bootstrap -->
