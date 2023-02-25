666d438e7672844a06ca4192995d00932da2595b
Lewis K Ycot <88181151+lewiskirori@users.noreply.github.com>
2023-02-25 04:08:01 -0800

    Update signin.php

--- signin.php
+++ signin.php
@@ -148,12 +148,12 @@ if($_POST) {
 						<form class="form-horizontal" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" id="loginForm">
 							<fieldset>
 							  <div class="form-group has-feedback">
-									  <input type="text" class="form-control" id="username" name="username" placeholder="Enter Username" autocomplete="off" />
+									  <input type="text" class="form-control" id="username" name="username" placeholder="Enter Username" autocomplete="on" />
 									  <span class="glyphicon glyphicon-user form-control-feedback"></span>
 								</div>
 								<div class="form-group has-feedback">
 							
-									  <input type="password" class="form-control" id="password" name="password" placeholder="Enter Password" autocomplete="off" />
+									  <input type="password" class="form-control" id="password" name="password" placeholder="Enter Password" autocomplete="on" />
 									  <span class="glyphicon glyphicon-lock form-control-feedback"></span>
 								</div>								
 								<div class="form-group">
