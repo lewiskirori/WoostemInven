82fed4ca63d8132d7b85a23fecb4f29269c28a47
Lewis Ycot <88181151+lewiskirori@users.noreply.github.com>
2023-04-12 23:09:22 -0700

    Add lead content desc

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Woostem | Amazing Stock Control</title>
     <meta name="description"
+	  content="Woostem An Amazing Stock Control System"/>
     <!--Inter UI font-->
     <link href="https://rsms.me/inter/inter-ui.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
