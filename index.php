5a16d407aac225ef05bbd4defe4f0ba7ac7a4d31
Lewis K Ycot <88181151+lewiskirori@users.noreply.github.com>
2023-04-25 10:36:07 +0630

    Update icons and request apis

--- index.php
+++ index.php
@@ -9,10 +9,8 @@
   <meta content="Woostem An Amazing A-grade Stock Control System" name="description">
   <meta content="" name="keywords">
 
-  <!-- Favicons -->
   <link href="assets/img/woo-icon.ico" rel="icon">
 
-  <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
@@ -23,7 +21,6 @@
   <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
   <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
-  <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
 </head>
 
