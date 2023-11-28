359220b1a4314fe6d228f46bdbe1b77b39ed1136
Lewis(K)Ycot <88181151+lewiskirori@users.noreply.github.com>
2023-11-28 10:35:23 +0300

    update control system

--- index.php
+++ index.php
@@ -185,7 +185,11 @@
                 <h3>About Woostem</h3>
                 <p>Woostem is a responsive database system created by DevL67. The control system is an open-source software, and can be managed by stockholders and owners with provision.</p>
                 <div class="social-links mt-3">
-                    <a href="https://twitter.com/" class="twitter"><i class="bx bxl-twitter"></i></a>
+                    <a href="https://x.com/" class="twitter">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" fill="currentColor" class="x-twitter" viewBox="0 0 512 512">
+                            <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/>
+                        </svg>
+                    </a>
                     <a href="https://facebook.com/" class="facebook"><i class="bx bxl-facebook"></i></a>
                     <a href="https://skype.com/" class="google-plus"><i class="bx bxl-skype"></i></a>
                     <a href="https://wa.me/254738400313" class="whatsapp"><i class="bx bxl-whatsapp"></i></a>
